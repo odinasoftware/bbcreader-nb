5bd0ed94173e950a69cd57a30f31656da90a651f
jae <jae@725052e4-4a61-44f1-851b-6ef29ce6032e>
2011-08-27 20:23:41 +0000

    When app goes to back ground, the socket can be in unknown state. When the accept fails, it should be reopened.
    git-svn-id: http://localhost:8090/svn/BBCReader2@849 725052e4-4a61-44f1-851b-6ef29ce6032e

--- Classes/LocalServerManager.m
+++ Classes/LocalServerManager.m
@@ -67,125 +67,68 @@ static LocalServerManager *sharedLocalServerManager = nil;
 	unsigned int clilen = 0;
 	int optval = 1;
 	int onoff = 0;
-	//int timeout = 1;
-	//struct timeval timeout;
-	//int rt=1;
-	//int sendBuf = 61440;
-	//int rcvBuf = 61440;
-	//int len=sizeof(ttl);
-	
-	//timeout.tv_sec = 1;
-	//timeout.tv_usec = 0;
-	
+	BOOL get_new_socket = NO;	
 	
 	
 	//TRACE("%s, ttl: %d\n", __func__, ttl);
 	
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	
-	/*
-	if (setsockopt(listenfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
-		TRACE("error in getsockopt: %s\n", strerror(errno));
-	}*/
-	/*
-	if (setsockopt(listenfd, SOL_SOCKET, SO_RCVBUF, &rcvBuf, sizeof(int)) < 0) {
-		NSLog(@"error in setsockopt: %s\n", strerror(errno));
-	}
-	
-	if (setsockopt(listenfd, SOL_SOCKET, SO_SNDBUF, &sendBuf, sizeof(int)) < 0) {
-		NSLog(@"error in setsockopt: %s\n", strerror(errno));
-	}*/
-	
-	 
-	//if (pipe(signal_pipe) < 0) {
-	//	NSLog(@"%s: %s", __func__, strerror(errno));
-	//	return;
-	//}
-	
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(LOCAL_PORT);
-	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-	if (bind(listenfd, (const struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-		NSLog(@"%s: %s", __func__, strerror(errno));
-		return;
-	}
-	
-	if (listen(listenfd, LISTENQ) < 0) {
-		NSLog(@"%s: %s", __func__, strerror(errno));
-		return;
-	}
-	
-	for (;;) {
-		@try {
-			/*
-			if (stopIt == YES) {
-				NSLog(@"Stop request has been detected.");
-				return;
-			}
-			 */
-			
-			//TRACE("~~~~ before accept ~~~~~~\n");
-			if ((connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen)) < 0) {
-				if (errno == EINTR)
-					continue;
-				else {
-					NSLog(@"%s, accept error: %s", __func__, strerror(errno));
-					continue;
-				}
-			}
-			
-			TRACE("LocalServer, accepting connection: %d\n", connfd);
-			//setsockopt(connfd, SOL_SOCKET, SO_LINGER, &l_onoff, sizeof(l_onoff));
-			if (setsockopt(connfd, IPPROTO_TCP, TCP_NODELAY, &onoff, sizeof(onoff)) < 0) {
-				NSLog(@"%s, failed to set nodelay. %s", __func__, strerror(errno));
-			}
-			/*
-			if (setsockopt(connfd, IPPROTO_TCP, TCP_MAXRT, (const void*)&timeout, sizeof(timeout)) < 0) {
-				NSLog(@"%s, failed to set timeout. %s", __func__, strerror(errno));
-			}*/
-			/*
-			if (setsockopt(connfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) < 0) {
-				TRACE("error in getsockopt: %s\n", strerror(errno));
-			}
-			 */
-			/*
-			if (setsockopt(connfd, SOL_SOCKET, SO_RCVTIMEO, (const void*)&timeout, sizeof(timeout)) < 0) {
-				NSLog(@"%s, failed to set timeout. %s", __func__, strerror(errno));
-			}
-			if (setsockopt(connfd, SOL_SOCKET, SO_SNDTIMEO, (const void*)&timeout, sizeof(timeout)) < 0) {
-				NSLog(@"%s, failed to set timeout. %s", __func__, strerror(errno));
-			}
-			*/
-			/*
-			if (setsockopt(connfd, SOL_SOCKET, SO_RCVBUF, &rcvBuf, sizeof(int)) < 0) {
-				NSLog(@"error in setsockopt: %s\n", strerror(errno));
-			}
-			
-			if (setsockopt(connfd, SOL_SOCKET, SO_SNDBUF, &sendBuf, sizeof(int)) < 0) {
-				NSLog(@"error in setsockopt: %s\n", strerror(errno));
-			}*/
-			
-			
-			
-			LocalServer *server = [[LocalServer alloc] initWithManager:self connFD:connfd];
-            TRACE("Starting thread: %p\n", server);
-			[server start];
-			[server release];
-			if (++activeThread >= MAX_LOCAL_SERVER_THREAD) {
-				[waitForThread lock];
-				TRACE("%s, waiting for available thread.\n", __func__);
-				[waitForThread wait];
-				[waitForThread unlock];
-				TRACE("%s, thread is available.\n", __func__);
-			}
-			
-		} @catch (NSException *exception) {
-			NSLog(@"%s: main: %@: %@", __func__, [exception name], [exception reason]);
-		}
-	}
-	
+    for (;;) {
+        listenfd = socket(AF_INET, SOCK_STREAM, 0);
+		
+        bzero(&servaddr, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servaddr.sin_port = htons(LOCAL_PORT);
+        setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+        if (bind(listenfd, (const struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+            NSLog(@"%s: %s", __func__, strerror(errno));
+            return;
+        }
+        
+        if (listen(listenfd, LISTENQ) < 0) {
+            NSLog(@"%s: %s", __func__, strerror(errno));
+            return;
+        }
+        
+        do {
+            @try {
+                              
+                //TRACE("~~~~ before accept ~~~~~~\n");
+                if ((connfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen)) < 0) {
+                    if (errno == EINTR)
+                        continue;
+                    else {
+                        NSLog(@"%s, accept error: %s", __func__, strerror(errno));
+                        get_new_socket = YES;
+                        continue;
+                    }
+                }
+                
+                TRACE("LocalServer, accepting connection: %d\n", connfd);
+                //setsockopt(connfd, SOL_SOCKET, SO_LINGER, &l_onoff, sizeof(l_onoff));
+                if (setsockopt(connfd, IPPROTO_TCP, TCP_NODELAY, &onoff, sizeof(onoff)) < 0) {
+                    NSLog(@"%s, failed to set nodelay. %s", __func__, strerror(errno));
+                }
+                         
+                LocalServer *server = [[LocalServer alloc] initWithManager:self connFD:connfd];
+                TRACE("Starting thread: %p\n", server);
+                [server start];
+                [server release];
+                if (++activeThread >= MAX_LOCAL_SERVER_THREAD) {
+                    [waitForThread lock];
+                    TRACE("%s, waiting for available thread.\n", __func__);
+                    [waitForThread wait];
+                    [waitForThread unlock];
+                    TRACE("%s, thread is available.\n", __func__);
+                }
+                
+            } @catch (NSException *exception) {
+                NSLog(@"%s: main: %@: %@", __func__, [exception name], [exception reason]);
+            }
+        } while (get_new_socket == NO);
+        close(listenfd);
+        NSLog(@"Close %d and restart socket.\n", listenfd);
+    }
 	
 }
 
